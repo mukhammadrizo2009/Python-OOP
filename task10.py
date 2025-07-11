1b7f0510314968837280b7da046b30aff9debac3
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-07-11 16:55:23 +0500

    Mission Complete

--- task10.py
+++ task10.py
@@ -1,22 +1,21 @@
 ### ✅ **Task 10 – BankAccount klassi bilan ishlash**
 
+
 class BankAccount:
-    def __init__(self , owner , amount ):
-        self.owner   = owner
-        self.balance = 0.0
-        
-    def deposit(self , amount , balance):
-            if amount > 0 :
-                amount += balance
-            else:
-                print("Mablag' noto'g'ri kiritildi!")
-                
-    def withdraw(self , balance , withdraw):
-            if withdraw <= balance and withdraw > 0:
-                balance -= withdraw
-            else:
-                print("Echmoqchi bo'lingan mablag' noto'g'ri kiritildi!")
-        
-user = BankAccount('Ali' , 67000)
-user.deposit(amount=67000 , balance=0)
-print(list(user.deposit()))
+    def __init__(self, owner, amount):
+        self.owner = owner
+        self.balance = amount  
+
+    def deposit(self, amount: float) -> None:
+        if amount > 0:
+            self.balance += amount
+
+    def withdraw(self, amount: float) -> None:
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
+
+user = BankAccount('Ali', 67000)
+
+user.deposit(amount=67000)
+
+print(user.balance) 
